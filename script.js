161e508493db29f6980f16158a686a9d15212dd8
Daniela523 <135756562+Daniela523@users.noreply.github.com>
2024-09-06 00:05:12 +0530

    script.js

--- script.js
+++ script.js
@@ -1,24 +1,11 @@
-function checkLoveMeter() {
-  const yourName = document.getElementById('yourName').value;
-  const partnerName = document.getElementById('partnerName').value;
+document.getElementById('love-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  const name1 = document.getElementById('name1').value;
+  const name2 = document.getElementById('name2').value;
 
-  if (!yourName || !partnerName) {
-    alert('Please enter both names.');
-    return;
+  if (name1 && name2) {
+    const url = `results.html?name1=${encodeURIComponent(name1)}&name2=${encodeURIComponent(name2)}`;
+    window.open(url, '_blank');
   }
-
-  const loveScore = Math.floor(Math.random() * 101);
-
-  alert('Please allow pop-ups in the next window. We can easily show your result if you allow it. If you don\'t allow, we are not able to show the result because the pop-up will be blocked.');
-  document.getElementById('adMessage').style.display = 'block';
-
-  setTimeout(() => {
-    const adWindow = window.open('https://www.highrevenuenetwork.com/dqpg3cak2?key=b3908da304a081bf9c86f27c84e9e0ee', '_blank');
-    if (adWindow) {
-      setTimeout(() => {
-        const resultURL = `result.html?yourName=${encodeURIComponent(yourName)}&partnerName=${encodeURIComponent(partnerName)}&loveScore=${loveScore}`;
-        window.open(resultURL, '_blank');
-      }, 30000);
-    }
-  }, 1000);
-}
+});
